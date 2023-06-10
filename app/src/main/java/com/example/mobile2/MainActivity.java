86588b3ee9e811a28e567faf9688df9dfb5c0c95
Chea-Pinglong <cheapinglong312@gmail.com>
2023-06-10 23:32:42 +0700

    all files

--- app/src/main/java/com/example/mobile2/MainActivity.java
+++ app/src/main/java/com/example/mobile2/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.mobile2;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.mobile2.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +26,106 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.buttomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.mnuHome){
+                showFragment(new HomeFragment());
+            }else if (item.getItemId() == R.id.mnuProvince){
+                showFragment(new ProvinceFragment());
+            }else if (item.getItemId() == R.id.mnuSearch) {
+                showFragment(new SearchFragment());
+            }else if (item.getItemId() == R.id.mnuProfile) {
+                showFragment(new ProfileFragment());
+            }else if (item.getItemId() == R.id.mnuMore) {
+                showFragment(new MoreFragment());
+            }
+
+            return true;
+        });
     }
+
+    private void showFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.lytFragment,fragment);
+
+        fragmentTransaction.commit();
+
+    }
+    private void showHomeFragment(){
+//        FragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+//        FragmentTransaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+//        Create fragment obj
+        HomeFragment homeFragment = new HomeFragment();
+
+//        Replace HomeFragment to lytFragment
+        fragmentTransaction.replace(R.id.lytFragment,homeFragment);
+
+//        commit transaction
+        fragmentTransaction.commit();
+    }
+
+
+
+    private void showProvinceFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ProvinceFragment provinceFragment = new ProvinceFragment();
+
+        fragmentTransaction.replace(R.id.lytFragment,provinceFragment);
+
+        fragmentTransaction.commit();
+    }
+
+    private void showProfileFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        ProfileFragment profileFragment = new ProfileFragment();
+
+        fragmentTransaction.replace(R.id.lytFragment,profileFragment);
+
+        fragmentTransaction.commit();
+    }
+
+
+
+    private void showSearchFragment(){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SearchFragment searchFragment = new SearchFragment();
+
+        fragmentTransaction.replace(R.id.lytFragment,searchFragment);
+
+        fragmentTransaction.commit();
+    }
+
+    private void showMoreFragment(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MoreFragment moreFragment = new MoreFragment();
+
+        fragmentTransaction.replace(R.id.lytFragment,moreFragment);
+
+        fragmentTransaction.commit();
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/mobile2/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.example.mobile2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mobile2.databinding.FragmentSearchBinding;
+
+public class SearchFragment extends Fragment {
+    private FragmentSearchBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        binding = FragmentSearchBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
